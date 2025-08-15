d045fc47afa2a0032b342736b249eb6452e32fb0
fpirotti <fpirotti@gmail.com>
2025-08-15 11:54:24 +0200

    cc

--- R/zzz.R
+++ R/zzz.R
@@ -20,6 +20,14 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
       )
       utils::install.packages("utils")
     }
+
+    if (!requireNamespace("h2o", quietly = FALSE)) {
+      packageStartupMessage(
+        "\u2705  Installo funzionalit\u00E0 H2O"
+      )
+      utils::install.packages("h2o", type = "source", repos = "http://h2o-release.s3.amazonaws.com/h2o/rel-3.44.0/3/R")
+    }
+
     packageStartupMessage(
       "\u2705 Verifico opzioni..."
     )
@@ -27,23 +35,20 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
     options(repos = c(CRAN = "https://cloud.r-project.org"))
     options(timeout = 600) # 10 minutes
 
-  if (!requireNamespace("pacman", quietly = FALSE)) {
+    if (!requireNamespace("pacman", quietly = FALSE)) {
+      packageStartupMessage(  "\u2705  Installo funzionalit\u00E0 pacman..." )
+      utils::install.packages("pacman")
+    }
 
     packageStartupMessage(
-      "\u2705  Installo funzionalit\u00E0 pacman..."
-    )
-    utils::install.packages("pacman")
-  }
-
-
-  packageStartupMessage(
     "\u2705 Verifico ulteriori dipendenze..."
-  )
+    )
+
   # List all required packages (CRAN or GitHub)
   required_cran <- c("leaflet", "cli", "shiny", "shinydashboardPlus",
                      "shinydashboard", "terra", "dplyr",
                      "sf", "lidR", "shinyjs", "cli",
-                     "leaflet.extras", "h2o", "shinyWidgets")
+                     "leaflet.extras",  "shinyWidgets")
 
   required_github <- c("fpirotti/CloudGeometry")
   availabl<- vapply(required_cran, requireNamespace, quietly = TRUE, FUN.VALUE = logical(1))
@@ -53,6 +58,7 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
     sprintf("%s %s ",
             required_cran2, required_cran)
   )
+
   if(any(!availabl)){
     packageStartupMessage(
       cli::col_red("Installo i seguenti elementi mancanti, potrebbe richiedere qualche minuto...."),
